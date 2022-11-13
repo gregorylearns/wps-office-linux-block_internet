1d0c7dae99eab1abffddf0fd297bf6808a13e3ea
Gregory <degeemon@gmail.com>
2022-11-13 16:32:39 +0800

    streamline to one script

--- firejail_wps.sh
+++ firejail_wps.sh
@@ -2,6 +2,20 @@
 # script to automatically change the exec parameter  on .desktop files on wps on
 # linux.
 
+## using firejail profiles
+
+WPS_apps_name=('et' 'wpp' 'wps' 'wpspdf')
+
+firejail_directory=~/.config/firejail
+mkdir -p $firejail_directory
+
+for app_name in "${WPS_apps_name[@]}"
+do
+	profile_dir="$firejail_directory"/"$app_name".profile
+	echo -e "net none\ninclude /etc/firejail/"$app_name".profile" > "$profile_dir"
+done
+
+## adding the parameter --net=none to the desktop files
 
 # /opt/kingsoft/wps-office/desktops/
 cd /opt/kingsoft/wps-office/desktops/
